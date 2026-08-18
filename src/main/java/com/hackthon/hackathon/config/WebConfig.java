17a4050e58dbaefc6c1c0d1a167626ab17b3b771
2sorae <sorae2222@naver.com>
2026-08-18 21:11:31 +0900

    past days 31

--- src/main/java/com/hackthon/hackathon/config/WebConfig.java
+++ src/main/java/com/hackthon/hackathon/config/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 프로젝트의 모든 주소(API)에 적용
-                .allowedOriginPatterns("*") // 모든 프론트엔드 주소 허용
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
